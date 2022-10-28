a6d75bc8b68970e0c5df9924e7115a2c03e71774
digitalplan <digitalplan21@gmail.com>
2022-10-28 11:09:48 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] array =
+                new int[][]{
+                        {1,   8,  12,  0,  4},
+                        {0,   2, 667, 32,  1},
+                        {0,   0,   0,  1,  3},
+                        {0, 355,   0,  2,  0},
+                        {0,  50,   6,  0,  1}
+        };
+
+        System.out.println(array[4][4]);
+    }
+}
